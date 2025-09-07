375ed171b1291e13f9688f31b690db46bfa4bf7d
n1o2h <aythmadnhylt@gmail.com>
2025-09-07 23:54:51 +0100

    Header file for the mini project containt structure and def of files

---
+++ Mini_Projet/livre.h
@@ -0,0 +1,33 @@
+#ifndef LIVRE_H
+#define LIVRE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Définition de la structure du livre
+typedef struct {
+    char titre[30];
+    char auteur[30];
+    float prix;
+    int quantite;
+} livre;
+
+// Ajouter un Livre au Stock
+void ajouter_livre(livre l[], int n);
+
+// Afficher Tous les Livres Disponibles
+void afficher_livre(livre l[], int n);
+
+//Rechercher un livre par son titre
+void rechercher_livre(livre l[], int n, char titre[30]);
+
+// Mettre à Jour la Quantité d'un Livre
+void mettre_quantite(livre *l, int n, char titr[30], int nov_quantite);
+
+// Supprimer un Livre du Stock
+void supprimer_livre(livre *l, int *n, char titre[30]);
+
+// Afficher le Nombre Total de Livres en Stock
+int nbr_Tlivre(livre l[], int n);
+
+#endif
